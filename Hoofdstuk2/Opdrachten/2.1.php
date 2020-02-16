48e027b118b679f61da9ac4640f4ca8521fc9a44
Nazim Ozdemir <nazim.ozdemir@edu-kw1c.nl>
2020-02-16 15:10:12 +0100

    push

---
+++ Hoofdstuk2/Opdrachten/2.1.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * User : Nazim Özdemir
+ * Date : 10-02-2020
+ * Time : 11:55
+ * File : 2.1.php
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+
+    </title>
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+    <link rel="index" type="text/html" href=""
+</head>
+
+<body>
+    <header>
+        <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+    </header>
+    <aside>
+        <h2>Menu</h2>
+            <ul>
+                <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="Opdracht%202.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+                </li>
+            </ul>
+
+        <ul>
+            <li>Hoofdstuk 3
+                <ul>
+                    <li>
+                        <a href="../Opdrachten/Opd">Opdracht 3.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 3.2</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 3.3</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
+        <ul>
+            <li>Hoofdstuk 4
+                <ul>
+                    <li>
+                        <a href="../Opdrachten/Opd">Opdracht 4.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 4.2</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 4.3</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 4.4</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
+        <ul>
+            <li>Hoofdstuk 5
+                <ul>
+                    <li>
+                        <a href="../Opdrachten/Opd">Opdracht 5.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 5.2</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 5.3</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 5.4</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
+        <ul>
+            <li>Eindopdracht Periode 3
+                <ul>
+                    <li>
+                        <a href="../Opdrachten/Opd">Eindopdracht</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <a href="../../index.php">Terug</a>
+
+    </aside>
+    <main id="wrapper">
+         <h2>Uitwerkingen</h2>
+    </main>
+    <p> <?php echo "Hello World!"; ?></p>
+
+</body>
+
+</html>
+
---
+++ index.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * User : Nazim Özdemir
+ * Date : 10-02-2020
+ * Time : 11:55
+ * File : 2.1.php
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+
+    </title>
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+</head>
+
+<body>
+
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="../../2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+    <ul>
+        <li>Hoofdstuk 3
+            <ul>
+                <li>
+                    <a href="../Opdrachten/Opd">Opdracht 3.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 3.2</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 3.3</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+    <ul>
+        <li>Hoofdstuk 4
+            <ul>
+                <li>
+                    <a hrer="">Opdracht 4.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 4.2</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 4.3</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 4.4</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+    <ul>
+        <li>Hoofdstuk 5
+            <ul>
+                <li>
+                    <a href="../Opdrachten/Opd">Opdracht 5.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 5.2</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 5.3</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 5.4</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+    <ul>
+        <li>Eindopdracht Periode 3
+            <ul>
+                <li>
+                    <a href="../Opdrachten/Opd">Eindopdracht</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <a href="index.php">Terug</a>
+
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
+
+</body>
+
+</html>
