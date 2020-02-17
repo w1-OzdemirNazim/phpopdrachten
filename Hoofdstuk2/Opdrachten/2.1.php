0301f1d10ddec661cb4638c1af7d89e47ca7586d
Nazim Ozdemir <nazim.ozdemir@edu-kw1c.nl>
2020-02-17 11:41:55 +0100

    push

--- Hoofdstuk2/Opdrachten/2.1.php
+++ Hoofdstuk2/Opdrachten/2.1.php
@@ -8,7 +8,7 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="nl">
 
 <head>
     <title>
@@ -27,82 +27,7 @@
     </header>
     <aside>
         <h2>Menu</h2>
-            <ul>
-                <li>Hoofdstuk 2
-            <ul>
-                <li>
-                    <a href="Opdracht%202.1.php">Opdracht 2.1</a>
-                </li>
-                <li>
-                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
-                </li>
-            </ul>
-                </li>
-            </ul>
 
-        <ul>
-            <li>Hoofdstuk 3
-                <ul>
-                    <li>
-                        <a href="../Opdrachten/Opd">Opdracht 3.1</a>
-                    </li>
-                    <li>
-                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 3.2</a>
-                    </li>
-                    <li>
-                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 3.3</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-
-        <ul>
-            <li>Hoofdstuk 4
-                <ul>
-                    <li>
-                        <a href="../Opdrachten/Opd">Opdracht 4.1</a>
-                    </li>
-                    <li>
-                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 4.2</a>
-                    </li>
-                    <li>
-                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 4.3</a>
-                    </li>
-                    <li>
-                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 4.4</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-
-        <ul>
-            <li>Hoofdstuk 5
-                <ul>
-                    <li>
-                        <a href="../Opdrachten/Opd">Opdracht 5.1</a>
-                    </li>
-                    <li>
-                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 5.2</a>
-                    </li>
-                    <li>
-                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 5.3</a>
-                    </li>
-                    <li>
-                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 5.4</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-
-        <ul>
-            <li>Eindopdracht Periode 3
-                <ul>
-                    <li>
-                        <a href="../Opdrachten/Opd">Eindopdracht</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
         <a href="../../index.php">Terug</a>
 
     </aside>
