42b8d1d557c1f88830a627e8ae3de6c4e8707789
Nazim Ozdemir <nazim.ozdemir@edu-kw1c.nl>
2020-03-29 21:22:11 +0200

    5.2 done

---
+++ Hoofdstuk5/Opdrachten/5.2.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * User: Nazim Özdemir
+ * Date: 23-3-2020
+ * Time: 15:00
+ * File: 5.2.php
+ */
+?>
+
+<?php
+include "../../Includes/header.php"
+?>
+
+    <h1>Uitschrijfformulier KW1C</h1>
+    <hr>
+    <form action="uitschrijving.php" method="get">
+        <label>Voor en achternaam</label>
+        <input name="Voor-Achternaam" type="text"><br>
+
+        <label>Studentennummer</label>
+        <input name="Studentennummer" type=number><br>
+
+        <label>Datum van uitschrijving</label>
+        <input name="DatumUitschrijving" type="text"><br>
+
+        <label>Reden van uitschrijving</label>
+        <select name="Reden">
+            <option value="Geen leuke opleiding">Geen leuke opleiding</option>
+            <option value="Te moeilijk">Te moeilijk</option>
+            <option value="Ik begrijp niks">Ik begrijp niks</option>
+            <option value="Veranderen van opleiding">Veranderen van opleiding</option>
+        </select><br>
+
+        <label>Leerjaar</label><br>
+        <input name="Leerjaar" type="radio">1e leerjaar</input><br>
+        <input name="Leerjaar" type="radio">2e leerjaar</input><br>
+        <input name="Leerjaar" type="radio">3e leerjaar</input><br>
+
+        <input name="Aanmelden" type="checkbox" value="ja">Ik wil me aanmeldden bij de succesklas<br>
+        <input name="Verwijder" type="checkbox" value="ja">Verwijder mijn gegevens uit het systeem<br>
+        <label>Geef hieronder de reden van je uitschrijving op</label><br>
+        <input name="Bericht" type="text"><br>
+
+        <input type="submit" name="verzenden" value="verzenden">
+    </form>
+
+
+<?php
+
+?>
+
+
+<?php
+include "../../Includes/footer.php"
+?>
---
+++ Hoofdstuk5/Opdrachten/uitschrijving.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * User: Nazim Özdemir
+ * Date: 29-3-2020
+ * Time: 15:00
+ * File: uitschrijving.php
+ */
+?>
+
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+
+    </title>
+
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <meta charset="utf-8">
+
+</head>
+<body>
+<h2>Uitwerking van <a href="../../index.php"> PHP-opdrachten</a></h2>
+<div class="box">
+
+    <h1>Uitschrijving KW1C</h1>
+    <hr>
+    <table>
+        <tr>
+            <th>Voor en achternaam</th>
+            <td><?php echo $_GET["Voor-Achternaam"]; ?></td>
+        </tr>
+        <tr>
+            <th>Studentennummer</th>
+            <td><?php echo $_GET["Studentennummer"]; ?></td>
+        </tr>
+        <tr>
+            <th>Datum van uitschrijving</th>
+            <td><?php echo $_GET["DaumUitschrijving"]; ?></td>
+        </tr>
+        <tr>
+            <th>Reden van uitschrijving</th>
+            <td><?php echo $_GET["Reden"]; ?></td>
+        </tr>
+        <tr>
+            <th>Leerjaar</th>
+            <td><?php echo $_GET["Leerjaar"]; ?></td>
+        </tr>
+        <tr>
+            <th>Aanmeldden bij de succesklas</th>
+            <td><?php echo $_GET["Aanmelden"]; ?></td>
+        </tr>
+        <tr>
+            <th>Verwijderen gegevens</th>
+            <td><?php echo $_GET["Verwijder"]; ?></td>
+        </tr>
+        <tr>
+            <th>Reden van uitschrijving</th>
+        </tr>
+        <tr>
+            <td><?php echo $_GET["Verwijder"]; ?></td>
+        </tr>
+    </table>
+
+
+<?php
+include "../../Includes/footer.php"
+?>
